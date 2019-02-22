0c18cf66481f4a3812cb5953b8f5dcbba911b834
Shawn Elliott <selliott@microsoft.com>
2019-02-22 12:58:29 -0500

    reinstated fixed version

--- EBusinessSuite/main.tf
+++ EBusinessSuite/main.tf
@@ -1,3 +1,4 @@
+/*
 locals {
   // VNET is /16
   subnet_bits = 8   # want 256 entries per subnet.
@@ -6,6 +7,93 @@ locals {
   lb_subnet_prefix      = "${cidrsubnet("${var.vnet_cidr}", 6, 1)}"
   app_subnet_prefix     = "${cidrsubnet("${var.vnet_cidr}", 6, 2)}"
   db_subnet_prefix      = "${cidrsubnet("${var.vnet_cidr}", 6, 3)}"
+}
+*/
+
+locals {
+    ## Key = subnet name, Value = CIDR prefixes e.g., 2.2.2.0/28)
+    subnetBits = 8   # want 256 entries per subnet.
+    # determine difference between VNET CIDR bits and that size subnetBits.
+    vnetCidrIncrease = "${32 - element(split("/",var.vnet_cidr),1) - local.subnetBits}"
+    subnetPrefixes = {
+        application     = "${cidrsubnet(var.vnet_cidr, local.vnetCidrIncrease, 1)}"
+        database        = "${cidrsubnet(var.vnet_cidr, local.vnetCidrIncrease, 2)}"
+        bastion         = "${cidrsubnet(var.vnet_cidr, local.vnetCidrIncrease, 3)}"
+     #   presentation    = "${cidrsubnet(var.vnet_cidr, local.vnetCidrIncrease, 4)}"
+     #   gateway         = "${cidrsubnet(var.vnet_cidr, local.vnetCidrIncrease, 5)}"
+    }
+
+    #####################
+    ## NSGs
+
+    bastion_sr_inbound = [
+        {   # SSH from outside
+            source_port_ranges = "*" 
+#TODO: Note that only one of prefix or prefixes is allowed and keywords can't be in the list.
+            source_address_prefix = "Internet"
+            destination_port_ranges =  "22" 
+        },{ # SSH from within any of the servers
+            source_port_ranges =  "*" 
+            source_address_prefix = "VirtualNetwork"
+            destination_port_ranges =  "22" 
+        } 
+    ]
+
+    bastion_sr_outbound = [
+        {  # SSH to any of the servers
+            source_port_ranges =  "*" 
+            source_address_prefix = "VirtualNetwork"
+            destination_port_ranges =  "22" 
+        }    
+    ]
+
+    application_sr_inbound = [
+        {
+            source_port_ranges =  "*" 
+            source_address_prefix = "AzureLoadBalancer"  # input from the Load Balancer only.             
+            destination_port_ranges = "8000" 
+            destination_address_prefix = "*"             
+        },
+        {
+            source_port_ranges =  "*" 
+            source_address_prefix = "${local.subnetPrefixes["bastion"]}"  # input from the Load Balancer only.               
+            destination_port_ranges = "22" 
+            destination_address_prefix = "*"             
+        }
+    ]
+
+    application_sr_outbound = [
+        {
+            source_port_ranges =  "*" 
+            source_address_prefix = "*" 
+            destination_port_ranges =  "1521"            
+            destination_address_prefix = "${local.subnetPrefixes["database"]}"  # out to DB
+        }
+        #TODO:
+        # outbound to file service
+    ]
+
+    database_sr_inbound = [
+        {
+            source_port_ranges =  "*" 
+            source_address_prefix = "${local.subnetPrefixes["application"]}"                 
+            destination_port_ranges =  "1521" 
+            destination_address_prefix = "*"             
+        },
+        {
+            source_port_ranges =  "*" 
+            source_address_prefix = "${local.subnetPrefixes["bastion"]}"  # input from the Load Balancer only.            
+            destination_port_ranges =  "22" 
+            destination_address_prefix = "*"                
+        }
+    ]
+    database_sr_outbound = [
+    ]
+
+    ##########################################
+    ## VMs in these subnets will need Availability sets.
+    ##########################################
+  #  needAVSets = [ "presentation", "application" ]
 }
 # Create Resource Group
 resource "azurerm_resource_group" "ebs-rg" {
