f82ff38e87b84a3df2abe7174531e982885a1b38
Jennelle Crothers <jkcrothers@hotmail.com>
2019-09-09 10:41:35 -0700

    Initial addition of app-gw

---
+++ Peoplesoft/modules/app_gateway/app_gw.variables.tf
@@ -0,0 +1,17 @@
+variable "location" {
+}
+
+variable "resource_group_name" {
+   
+}
+
+variable "prefix" {
+}
+
+variable "frontend_subnet_id" {}
+
+variable "vnet_name" {}
+
+variable "lb_frontend_ips" {
+    type = "list"
+}
