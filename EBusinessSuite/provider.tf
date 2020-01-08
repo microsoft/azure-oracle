fd436e3b818e09fa00303b4582ac2aadadf8365f
Romit Girdhar <romitvgirdhar@gmail.com>
2020-01-08 15:04:48 -0800

    adding partner_id for EBS

--- EBusinessSuite/provider.tf
+++ EBusinessSuite/provider.tf
@@ -1,12 +1,20 @@
 #########################
 ## Azure ARM Provider
 #########################
+
+# Terraform version
+
+terraform {
+  required_version = ">= 0.11.11"
+}
+
 provider "azurerm" {
     version= ">=1.21.0"
     tenant_id       = "${var.tenant_id}"
     subscription_id = "${var.subscription_id}"
     client_id       = "${var.client_id}"
-    client_secret   = "${var.client_secret}"   
+    client_secret   = "${var.client_secret}"
+    partner_id      = "3662be22-8a0f-5a88-a9ef-f3ee6c43e241"
 }
 
 provider "random" {
